29f5c0b5405094b739125ae08d2885a1a1c0da5e
USER <USER@192.168.1.3>
2025-03-25 20:29:48 +0530

    just

--- groceryapplication/src/main/java/utility/FileUploadUtility.java
+++ groceryapplication/src/main/java/utility/FileUploadUtility.java
@@ -12,31 +12,30 @@ import org.openqa.selenium.WebElement;
 
 public class FileUploadUtility {
 //public WebDriver driver;
-	public void fileUploadUsingSendKeys( WebElement element, String filepath) 
-	{
+	public void fileUploadUsingSendKeys(WebElement element, String filepath) {
 		element.sendKeys(filepath);
-		
-	}
-	//RoboKeys
-	public void fileUploadUsingRoboKey( WebElement element, String filepath) throws AWTException
-	
-	{
-		element.click();;
-				
- 		StringSelection ss=new StringSelection(filepath); 
- 		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null); 
- 	  	  	Robot robot = new Robot(); 
- 		     robot.delay(500); 
- 		     robot.keyPress(KeyEvent.VK_CONTROL); 
- 		     robot.keyPress(KeyEvent.VK_V); 
- 		  	 robot.delay(500); 
- 		     robot.keyRelease(KeyEvent.VK_CONTROL); 
- 		     robot.keyRelease(KeyEvent.VK_V); 
- 		     robot.delay(500); 
- 		     robot.keyPress(KeyEvent.VK_ENTER); 
- 		     robot.keyRelease(KeyEvent.VK_ENTER);
-		
-	}
-			
-}
 
+	}
+
+	// RoboKeys
+	public void fileUploadUsingRoboKey(WebElement element, String filepath) throws AWTException
+
+	{
+		element.click();
+
+		StringSelection ss = new StringSelection(filepath);
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+		Robot robot = new Robot();
+		robot.delay(500);
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.delay(500);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.keyRelease(KeyEvent.VK_V);
+		robot.delay(500);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+
+	}
+
+}
